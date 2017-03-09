d59a55866ebeaf5973ec88a5e76612391d8a8a35
Johan Sahlén <johan@monospace.se>
2017-03-09 15:26:11 +0100

    Need Promises for cutting the mustard

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,7 @@
 import * as controllers from 'controllers'
 
 // Check if browser cuts the mustard
-if ('querySelector' in document && 'classList' in document.documentElement) {
+if ('querySelector' in document && 'classList' in document.documentElement && 'Promise' in window) {
   Object.keys(controllers).forEach(key => {
     let controller = controllers[key]
 
